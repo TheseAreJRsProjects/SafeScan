07cddbea4ea1692c08f72a459f098e570b397aca
TheseAreJRsProjects <jrhidalgo722@gmail.com>
2026-08-22 11:56:21 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('clean-scan-v1').then(cache => {
+      return cache.addAll(['/', '/index.html', '/styles.css', '/app.js']);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
